98532e8f8380a812bd3d797c739bf218b4e89394
Joseph Thomas-Kerr <joseph@ubookr.com>
2019-11-11 10:06:04 +1100

    Implement MonetaryAmount methods

--- src/me/jvlk/dius/store/models/MonetaryAmount.java
+++ src/me/jvlk/dius/store/models/MonetaryAmount.java
@@ -10,18 +10,18 @@ public class MonetaryAmount {
     private int cents;
 
     public MonetaryAmount(double value) {
-
+        this.cents = (int) (value * 100);
     }
 
     public MonetaryAmount vary(double ratio) {
-        return null;
+        return new MonetaryAmount((cents * ratio)/100);
     }
 
     // FUTURE - gst settings
     // FUTURE - support multiple currencies - issues: conversion, validation, time differences?
 
     public static MonetaryAmount sum(Collection<MonetaryAmount> amounts) {
-        return null;
+        return amounts.stream().reduce(FREE, (a, b) -> new MonetaryAmount((a.cents + b.cents)/100D));
     }
 
     public static final MonetaryAmount FREE = new MonetaryAmount(0);
