084a0adc36d52fd96513d4c1f2d1f28469de23cf
Joseph Thomas-Kerr <joseph@ubookr.com>
2019-11-12 11:01:21 +1100

    Implement checkout

--- src/me/jvlk/dius/store/Checkout.java
+++ src/me/jvlk/dius/store/Checkout.java
@@ -1,6 +1,7 @@
 package me.jvlk.dius.store;
 
 import me.jvlk.dius.store.models.MonetaryAmount;
+import me.jvlk.dius.store.models.Priced;
 import me.jvlk.dius.store.models.PricingRule;
 import me.jvlk.dius.store.models.Product;
 import me.jvlk.dius.store.persistence.PricingRuleBuilder;
@@ -9,27 +10,35 @@ import me.jvlk.dius.store.persistence.rules.FreeGiftBuilder;
 import me.jvlk.dius.store.persistence.rules.VolumePricingBuilder;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static me.jvlk.dius.store.Utils.strToDate;
 
 public class Checkout {
-    /**
-     * This is not at all a robust mechanism for instantiating products, it is done this way only to comply with the required interface
-     */
-    private static final Collection<Product> DEFAULT_PRODUCTS = null;
+    private static final Date OPENING_DAY = strToDate("12-11-2019");
+
+    private final Collection<PricingRule> pricingRules;
+    private final List<Priced> cart;
 
     public Checkout(Collection<PricingRule> pricingRules) {
-        this(pricingRules, DEFAULT_PRODUCTS);
+
+        this.pricingRules = pricingRules;
+        this.cart = new LinkedList<>();
     }
 
-    public Checkout(Collection<PricingRule> pricingRules, Collection<Product> products) {
-
-    }
-
-    void scan(String sku) throws UnknownItemException {
-
+    public void scan(String sku) throws UnknownItemException {
+        Product product = ProductStore.getInstance().findProduct(sku);
+        if (product == null) throw new UnknownItemException(sku);
+        cart.add(product);
     }
 
     public MonetaryAmount total() {
-        return null;
+        List<Priced> priceds = PricingRule.applyAll(pricingRules, OPENING_DAY, cart);
+        List<MonetaryAmount> amounts = priceds.stream().map(Priced::getPrice).collect(toList());//FIXME -should do this streaming
+        return MonetaryAmount.sum(amounts);
     }
 
     /**
